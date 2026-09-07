83703adfc9bbc4f95542fd6e33f02c9a991119ff
Matheus Boas Azevedo Alves <matheusalves9001@outlook.com>
2026-09-07 12:03:14 -0300

    Reformulacao do codigo

--- Potenciometro.ino
+++ Potenciometro.ino
@@ -22,84 +22,68 @@ void loop() {
   int valorRecebido = analogRead(pot);
   float tensao = valorRecebido * (5.0 / 1023.0);
   int faixa = map(valorRecebido, 0, 1024, 0, 6);
-  ligarLeds(faixa, buzzer);
-  tocarBuzzer(faixa);
+  medidor(faixa);
   Serial.print("Tensao: ");
-  Serial.println(tensao, 2);
+  Serial.print(tensao, 2);
+  Serial.print(" | Faixa: ");
+  Serial.println(faixa);
   delay(1000);
 }
-void ligarLeds(int faixa, int buzzer){
-  if(faixa == 0){
+void medidor(int faixa){
+  switch (faixa) {
+  case 0:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, LOW);
       digitalWrite(ledAmareloI, LOW);
       digitalWrite(ledAmareloII, LOW);
       digitalWrite(ledVermelhoI, LOW);
       digitalWrite(ledVermelhoII, LOW);
-  }
-  else if(faixa == 1){
+      noTone(buzzer);
+    break;
+  case 1:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, HIGH);
       digitalWrite(ledAmareloI, LOW);
       digitalWrite(ledAmareloII, LOW);
       digitalWrite(ledVermelhoI, LOW);
       digitalWrite(ledVermelhoII, LOW);
-  }
-  else if(faixa == 2){
+      tone(buzzer, 262);
+    break;
+  case 2:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, HIGH);
       digitalWrite(ledAmareloI, HIGH);
       digitalWrite(ledAmareloII, LOW);
       digitalWrite(ledVermelhoI, LOW);
       digitalWrite(ledVermelhoII, LOW);
-  }
-  else if(faixa == 3){
+      tone(buzzer, 330);
+    break;
+  case 3:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, HIGH);
       digitalWrite(ledAmareloI, HIGH);
       digitalWrite(ledAmareloII, HIGH);
       digitalWrite(ledVermelhoI, LOW);
       digitalWrite(ledVermelhoII, LOW);
-  }
-  else if(faixa == 4){
+      tone(buzzer, 392);
+    break;
+  case 4:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, HIGH);
       digitalWrite(ledAmareloI, HIGH);
       digitalWrite(ledAmareloII, HIGH);
       digitalWrite(ledVermelhoI, HIGH);
       digitalWrite(ledVermelhoII, LOW);
-  }
-  else if(faixa == 5){
+      tone(buzzer, 523);
+    break;
+  case 5:
       digitalWrite(ledVerdeI, HIGH);
       digitalWrite(ledVerdeII, HIGH);
       digitalWrite(ledAmareloI, HIGH);
       digitalWrite(ledAmareloII, HIGH);
       digitalWrite(ledVermelhoI, HIGH);
       digitalWrite(ledVermelhoII, HIGH);
-  }
-}
-void tocarBuzzer(int faixa){
-  switch (faixa) {
-  case 0:
-    noTone(buzzer);
-    break;
-  case 1:
-    tone(buzzer, 262); // Nota Dó
-    break;
-  case 2:
-    tone(buzzer, 330); // Nota Mi
-    break;
-  case 3:
-    tone(buzzer, 392); // Nota Sol
-    break;
-  case 4:
-    tone(buzzer, 523); // Nota Dó (oitava acima)
-    break;
-  case 5:
-    tone(buzzer, 659);
-    break;
-  case 6:
-    tone(buzzer, 784);
+      tone(buzzer, 659);
     break;
 }
 }
